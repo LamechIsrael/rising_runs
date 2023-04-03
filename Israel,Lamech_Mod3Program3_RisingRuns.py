08736a7529d4b0719a5ee8b704c3a403baabb941
Lamech A Israel <103595802+LamechIsrael@users.noreply.github.com>
2023-04-03 09:45:03 -0500

    Add files via upload

---
+++ Israel,Lamech_Mod3Program3_RisingRuns.py
@@ -0,0 +1,62 @@
+#############################################
+# CS 1110A - Programming in Python          #
+# Module 3 - Project 3 - Rising Runs        #
+# Author: Lamech Israel                     #
+# Date:   01/29/2023                        #
+#############################################
+
+
+def rising_runs(s):
+    run1 = 1
+    run2 = 0
+    
+    if len(s) == 0:
+        return False
+    else:
+        for i in range(len(s)-1):
+            # If the letter is the same as the next, add 1 to the first run
+            if s[i] == s[i+1] and i != len(s)-2:
+                run1 += 1
+  
+            # If the letter is not the same as the next, execute this function
+            else:
+                
+                # If the reason we're here is this is the second to last character, execute this
+                if i == len(s)-2:
+                    # If the second-to-last character is the same as the last, add 1
+                    if s[i] == s[i+1]:
+                        run1+=1
+                    # Else, make conditions to exit code
+                    else:
+                        run2 = run1
+                        run1 = 1
+                    
+                
+                # If this run is greater than the last run, store the new run
+                if run1 >= run2:
+                    run2 = run1
+                    run1 = 1
+
+                
+                # Else, end the string and tell the user
+                else:
+                    print('This string is not rising')
+                    
+                    return True
+
+        
+        print('This string is rising')
+        return True 
+
+# Begin Program
+Running = True
+
+print('Does a string contain rising runs?')
+
+while Running:
+    
+    Running = rising_runs(input('\nEnter a string: '))
+    
+print('\nDone!\n')
+
+exit()
